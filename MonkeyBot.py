8186c0791e0ad984fedb7aaf3540da731b9c37f1
Sai Praneth Raju K <sai.praneth10@gmail.com>
2023-10-27 22:47:40 -0700

    Updated Bot
    Added new xpath location for updated monkeytype.com website, and updated requirements.txt with new selenium 4.14.0

--- MonkeyBot.py
+++ MonkeyBot.py
@@ -12,12 +12,12 @@ TIMEINT_ERR = 0.02   # 0.05 +- 0.02
 TYPOS_RATE = 0.20    # 15% percent error
 TIMECONTROL = 30     # Gamemode in monkeytype
 
-service = Service(executable_path='./chromedriver.exe')
+service = Service(executable_path='chromedriver.exe')
 driver = webdriver.Chrome(options=Options(), service=service)
 
 # get the browser ready
 driver.get('https://monkeytype.com/')
-button = driver.find_element(by=By.XPATH, value='//*[@id="cookiePopup"]/div[2]/div[2]/div[1]')
+button = driver.find_element(by=By.XPATH, value='//*[@id="cookiePopup"]/div[2]/div[2]/button[1]')
 driver.execute_script('arguments[0].click()', button)
 time.sleep(0.5)
 
